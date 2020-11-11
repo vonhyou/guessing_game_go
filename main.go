0108272ccbc013b13b3abe5555362e67a313cf53
vonhyou <vonhyou@yeah.net>
2020-11-11 21:25:47 +0800

    make core function

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("hello, world!")
+  fmt.Println("GUESSING GAME")
+
+  var guess int
+  var secretNumber, life = 42, 10
+
+  for life > 0 {
+    fmt.Print("Input your number:")
+    fmt.Scanln(&guess)
+    if guess < secretNumber {
+      fmt.Println("TOO SMALL!")
+    } else if guess > secretNumber {
+      fmt.Println("TOO BIG")
+    } else {
+      fmt.Println("YOU WON!")
+      break
+    }
+    life--
+  }
+  if life == 0 {
+    fmt.Println("YOU LOSE!")
+  }
 }
